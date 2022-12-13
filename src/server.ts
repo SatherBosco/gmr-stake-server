8a294f5e4d1f2797303ef8b7f835e597ea4d6f4a
Sather Bosco <satherbosco@hotmail.com>
2022-12-13 00:21:07 -0300

    distr novembro

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ import MigrateStakeController from "./app/controllers/migrateStakeController";
 new App().server.listen(3333, () => console.log("Server is running! Port: 3333"));
 
 const listenToEvents = () => {
-    const NODE_URL = "https://side-falling-tent.bsc.discover.quiknode.pro/e979073a7ea17521eca5788e3abdcd2170cff16d/";
+    const NODE_URL = "https://quiet-polished-sheet.bsc.discover.quiknode.pro/8b45b0b83e386fa8eba0930f8a35897fafcce8a4/";
     const provider = new ethers.providers.JsonRpcProvider(NODE_URL);
 
     const oldStakeObj = new ethers.Contract("0x26Db0a816699a87a57F6E451364C500f7E229a7E", OldStake, provider);
